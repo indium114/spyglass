0c03fcd37b8c84f3fb2e99142c62c985e67bc045
indium114 <156162907+indium114@users.noreply.github.com>
2026-06-29 13:35:05 +0200

    feat: basic ratatui rendering

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+use ratatui::{
+    DefaultTerminal, Frame,
+    crossterm::event::{self, Event, KeyEventKind},
+};
+
+fn main() -> color_eyre::Result<()> {
+    color_eyre::install()?;
+    ratatui::run(app)?;
+    Ok(())
+}
+
+fn app(terminal: &mut DefaultTerminal) -> std::io::Result<()> {
+    loop {
+        terminal.draw(render)?;
+        match event::read()? {
+            Event::Key(key_event) if key_event.kind == KeyEventKind::Press => {
+                match key_event.code {
+                    event::KeyCode::Esc => break,
+                    _ => (),
+                }
+            }
+            _ => (),
+        }
+    }
+
+    Ok(())
+}
+fn render(frame: &mut Frame) {
+    frame.render_widget("Hello, world!", frame.area());
 }
