d6ab8718dd6aa988acb3ee58805f99978d8616d3
indium114 <156162907+indium114@users.noreply.github.com>
2026-06-29 13:49:54 +0200

    feat: mockup UI

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use ratatui::{
     DefaultTerminal, Frame,
     crossterm::event::{self, Event, KeyEventKind},
+    layout::{Constraint, Direction, Layout},
+    widgets::{Block, Borders, Paragraph},
 };
 
 fn main() -> color_eyre::Result<()> {
@@ -26,5 +28,30 @@ fn app(terminal: &mut DefaultTerminal) -> std::io::Result<()> {
     Ok(())
 }
 fn render(frame: &mut Frame) {
-    frame.render_widget("Hello, world!", frame.area());
+    let layout = Layout::default()
+        .direction(Direction::Vertical)
+        .constraints(vec![
+            Constraint::Length(3),
+            Constraint::Fill(1),
+            Constraint::Length(5),
+            Constraint::Length(3),
+        ])
+        .split(frame.area());
+
+    frame.render_widget(
+        Paragraph::new("[Lens 1] | Lens 2 | Lens 3 | Lens 4").block(Block::new().borders(Borders::ALL)),
+        layout[0],
+    );
+    frame.render_widget(
+        Paragraph::new("Result 1\nResult 2\nResult 3\nResult 4\nResult 5").block(Block::new().borders(Borders::ALL)),
+        layout[1],
+    );
+    frame.render_widget(
+        Paragraph::new("This is a description. The quick brown fox jumps over the lazy dog").block(Block::new().borders(Borders::ALL)),
+        layout[2],
+    );
+    frame.render_widget(
+        Paragraph::new("> Search").block(Block::new().borders(Borders::ALL)),
+        layout[3],
+    );
 }
