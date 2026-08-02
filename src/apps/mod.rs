e0e5f0d872546b5369f610dcab6d66ee90b3e40e
indium114 <156162907+indium114@users.noreply.github.com>
2026-08-02 10:43:56 +0200

    chore: change apps# config dir to regular applications dir

--- src/apps/mod.rs
+++ src/apps/mod.rs
@@ -19,7 +19,7 @@ fn load() -> Vec<AppConfig> {
     let dir = dirs::config_dir()
         .map(|s| s.to_string_lossy().into_owned())
         .unwrap_or_default()
-        + "/spyglass/applications_dev/"; // TODO: change this to regular `applications` dir
+        + "/spyglass/applications/";
 
     for file in fs::read_dir(dir).unwrap() {
         let file = file.unwrap().path();
